49acf6679ab2b37d13c19188e5e5dee419492fd6
DanLisow <56221679+DanLisow@users.noreply.github.com>
2020-05-25 17:13:03 +0300

    Add files via upload

---
+++ Snake 2.0/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Snake_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(712, 473);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("FuturaMediumC", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(733, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(81, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 473);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Snake 2.0/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_2._0
+{
+    public partial class Form1 : Form
+    {
+
+        Point[] snake;
+        Point apple;
+        SolidBrush brush;
+        SolidBrush snakeBrush;
+        SolidBrush appleBrush;
+        SolidBrush headBrush;
+        Random rand;
+        string direction = "up";
+        int length = 1;
+        int width, height;
+        int count = 0;
+        
+
+        public Form1()
+        {
+            rand = new Random();
+            snake = new Point[10000];
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            width = pictureBox1.Width / 10;
+            height = pictureBox1.Height / 10;
+            snake[0].X = width / 2;
+            snake[0].Y = height / 2;
+
+            brush = new SolidBrush(Color.White);
+            snakeBrush = new SolidBrush(Color.Blue);
+            appleBrush = new SolidBrush(Color.Green);
+            headBrush = new SolidBrush(Color.Crimson);
+
+            apple.X = rand.Next(0, width - 1);
+            apple.Y = rand.Next(0, height - 1);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Graphics graphic = Graphics.FromImage(pictureBox1.Image);
+            graphic.FillRectangle(brush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+
+            if (length > 3)
+            {
+                for (int i = 1; i < length; i++)
+                {
+                    for (int j = i + 1; j < length; j++)
+                    {
+                        if(snake[i].X == snake[j].X && snake[i].Y == snake[j].Y)
+                        {
+                            timer1.Stop();
+                            MessageBox.Show("Вы проиграли");
+                            Application.Exit();
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+
+                if (snake[i].X < 0) snake[i].X += width;
+                if (snake[i].X > width) snake[i].X -= width + 1;
+                if (snake[i].Y < 0) snake[i].Y += height;
+                if (snake[i].Y > height) snake[i].Y -= height + 1;
+
+                graphic.FillEllipse(snakeBrush, snake[i].X * 10, snake[i].Y * 10, 10, 10);
+
+                
+                if(apple.X == snake[i].X && apple.Y == snake[i].Y)
+                {
+                    apple.X = rand.Next(0, width - 1);
+                    apple.Y = rand.Next(0, height - 1);
+                    length++;
+                    timer1.Interval -= 1;
+                    label1.Text = "Score: " + ++count;
+                }
+
+            }
+
+            graphic.FillEllipse(appleBrush, apple.X * 10, apple.Y * 10, 10, 10);
+            graphic.FillEllipse(headBrush, snake[0].X * 10, snake[0].Y * 10, 10, 10);
+
+            createSnake();
+            moveSnake();
+
+            if (length > 10000 - 3)
+            {
+                length = 10000 - 3;
+            }
+
+            pictureBox1.Invalidate();
+        }
+
+        public void moveSnake()
+        {
+            if (direction == "up") snake[0].Y -= 1;
+            if (direction == "down") snake[0].Y += 1;
+            if (direction == "right") snake[0].X += 1;
+            if (direction == "left") snake[0].X -= 1;
+        }
+
+        public void createSnake()
+        {
+            for (int i = length; i >= 0; i--)
+            {
+                snake[i + 1].X = snake[i].X;
+                snake[i + 1].Y = snake[i].Y;
+            }
+
+            if (length < 3) length++;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                direction = "up";
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                direction = "down";
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                direction = "right";
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                direction = "left";
+            }
+        }
+    }
+}
